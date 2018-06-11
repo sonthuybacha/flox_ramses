dd0043a6872c00f79d34064321bd1dab56ce42a0
Atreya Shankar <shankar.atreya@gmail.com>
2018-06-11 22:36:05 +0200

    updated R file

--- RCleanData.R
+++ RCleanData.R
@@ -11,12 +11,24 @@ for(i in 1:(length(megalist)/4)){
 
 #### create useful functions ####
 
+cleanUp <- function(y){
+  x <- y[,1]
+  y <- as.data.frame(t(y[,-1]))
+  colnames(y) <- x
+  rownames(y) <- NULL
+  if(length(which(rowSums(is.na(y)) > 0) > 0)) {
+    stop("error, please remove NA-filled rows and try again")
+  }
+  return(y)
+}
+
 nLines <- function(file){
   testcon <- file(file,open="r")
   readsizeof <- 20000
   nooflines <- 0
-  (while((linesread <- length(readLines(testcon,readsizeof))) > 0 )
-    nooflines <- nooflines+linesread )
+  while((linesread <- length(readLines(testcon,readsizeof))) > 0){
+    nooflines <- nooflines+linesread
+  }
   close(testcon)
   return(nooflines)
 }
@@ -32,7 +44,7 @@ disect <- function(file){
     myValues[[i]] <- read.csv(file, sep = ";", skip = a, nrows = 5, header = F, stringsAsFactors = FALSE)
     x <- myValues[[i]][,1]
     myValues[[i]] <- as.data.frame(t(myValues[[i]][,-1]))
-    colnames(myValues[[i]]) <- x
+    colames(myValues[[i]]) <- x
     myValues[[i]] <- myValues[[i]][-which(rowSums(is.na(myValues[[i]])) == ncol(myValues[[i]])),]
     rownames(myValues[[i]]) <- NULL
   }
@@ -48,10 +60,22 @@ disect <- function(file){
   return(result)
 }
 
+#### create additional variables ####
+
+cal.fine <- read.csv(paste(getwd(),"/Data/00_CAL_Fine.csv", sep=""), sep = ";", header = F, stringsAsFactors = FALSE)
+cal.fine <- cleanUp(cal.fine)
+
+cal.full <- read.csv(paste(getwd(),"/Data/00_CAL_Full.csv", sep=""), sep = ";", header = F, stringsAsFactors = FALSE)
+cal.full <- cleanUp(cal.full)
+
 #### create workflows per 4 main entries in megalist ####
 
+for(i in 1:length(myList[[1]])){
+  
+}
 
 
+# for calculations, use grep and find entry after relevant text to get values
 
 #### extra code ####
 
