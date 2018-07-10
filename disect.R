88efa5905cf857456ff2722626b212ce685209bf
Atreya Shankar <35427332+AtreyaSh@users.noreply.github.com>
2018-07-10 13:52:20 +0200

    str changes

--- disect.R
+++ disect.R
@@ -6,17 +6,16 @@ disect <- function(file){
   
   for(i in 1:dim){
     a <- (6*i-5)
+    b <- (6*i-6)
+    
     myValues[[i]] <- read.csv(file, sep = ";", skip = a, nrows = 5, header = F, stringsAsFactors = FALSE)
     x <- myValues[[i]][,1]
     myValues[[i]] <- as.data.frame(t(myValues[[i]][,-1]))
     colnames(myValues[[i]]) <- x
     myValues[[i]] <- myValues[[i]][-which(rowSums(is.na(myValues[[i]])) == ncol(myValues[[i]])),]
     rownames(myValues[[i]]) <- NULL
-  }
-  
-  for(i in 1:dim){
-    a <- (6*i-6)
-    myDetails[[i]] <- read.csv(file, sep = ";", skip = a, nrows = 1, header = F, stringsAsFactors = FALSE)
+    
+    myDetails[[i]] <- read.csv(file, sep = ";", skip = b, nrows = 1, header = F, stringsAsFactors = FALSE)
     myDetails[[i]] <- as.data.frame(t(myDetails[[i]]), stringsAsFactors = FALSE)
     rownames(myDetails[[i]]) <- NULL
   }
