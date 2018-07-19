761408b320dba4b5f61e03b87ed0ad58ddc66ad0
Atreya Shankar <35427332+AtreyaSh@users.noreply.github.com>
2018-07-19 14:55:42 +0200

    major changes

--- RCleanData.R
+++ RCleanData.R
@@ -4,6 +4,8 @@ megalist <- list.files(paste(getwd(), "/Data", sep=""), full.names=TRUE)
 megalist <- megalist[-c(1:3, 100)]
 megalist <- megalist[-c(5,6,55,56)]
 myList <- list()
+final.results.fine <- list()
+final.results.full <- list()
 p = 0.0245
 
 for(i in 1:(length(megalist)/4)){
@@ -24,13 +26,24 @@ cal.full <- cleanUp(cal.full)
 
 #### create workflows per 4 main entries in megalist ####
 
+pb.overall <- txtProgressBar(min = 0, max = length(myList), initial = 0, char = "=",
+                             width = NA, title, label, style = 3, file = "")
+
+pb.fine <- txtProgressBar(min = 0, max = length(myList), initial = 0, char = "=",
+                             width = NA, title, label, style = 3, file = "")
+
+pb.full <- txtProgressBar(min = 0, max = length(myList), initial = 0, char = "=",
+                             width = NA, title, label, style = 3, file = "")
+
+start.time <- Sys.time()
+
 for(i in 1:length(myList)){
   
-  result.fine <- data.frame(matrix(ncol = 7))
-  names(result.fine) <- c("LuFrac", "LskyFrac", "EdFrac1", "EdFrac2", "result.fine.1", "result.fine.2", "wl")
+  result.fine <- data.frame(matrix(ncol = 11))
+  names(result.fine) <- c("luFrac", "lskyFrac", "edFrac1", "edFrac2", "result.fine.1", "result.fine.2", "wl", "date", "time", "lat", "lon")
   
-  result.full <- data.frame(matrix(ncol = 7))
-  names(result.full) <- c("LuFrac", "LskyFrac", "EdFrac1", "EdFrac2", "result.full.1", "result.full.2", "wl")
+  result.full <- data.frame(matrix(ncol = 11))
+  names(result.full) <- c("luFrac", "lskyFrac", "edFrac1", "edFrac2", "result.full.1", "result.full.2", "wl", "date", "time", "lat", "lon")
   
   # can add new lsky possibilities here, but exception of last case
   
@@ -63,11 +76,15 @@ for(i in 1:length(myList)){
     checkIndex.fine <- 1
     checkIndex.full <- 1
   }
-    
+  
+  w <- length(final.results.fine)
+  
   # for fine
   for(j in 1:min(c(length(lu.fine[[1]]), length(lsky.fine[[1]])))){
     for(k in 1:1024){
+      
       lufrac <- (lu.fine[[1]][[j]][k,2]-lu.fine[[1]][[j]][k,5])/(as.numeric(lu.fine[[2]][[j]][which(lu.fine[[2]][[j]] == "IT_VEG[us]=")+1,])*cal.fine[k,2])
+      
       result.fine[k,1] <- lufrac
       
       if(checkIndex.fine == 1){
@@ -91,13 +108,30 @@ for(i in 1:length(myList)){
       result.fine[k,6] <- result2
       
       result.fine[k,7] <- cal.fine[k,1]
+      
+      date <- lu.fine[[2]][[j]][which(lu.fine[[2]][[j]] == "GPS_date=")+1,]
+      time <- lu.fine[[2]][[j]][which(lu.fine[[2]][[j]] == "GPS_TIME_UTC=")+1,]
+      
+      result.fine[k,8] <- date
+      
+      result.fine[k,9] <- time
+      
+      lat <- lu.fine[[2]][[j]][which(lu.fine[[2]][[j]] == "GPS_lat=")+1,]
+      result.fine[k,10] <- lat
+      
+      lon <- lu.fine[[2]][[j]][which(lu.fine[[2]][[j]] == "GPS_lon=")+1,]
+      result.fine[k,11] <- lon
     }
-    write.csv(result.fine, file = paste(getwd(), "/Results/", "results_fine_", i ,"_", j, ".csv", sep=""), row.names = FALSE)
+    m = j + w
+    final.results.fine[[m]] <- result.fine
   }
+  
+  w <- length(final.results.full)
   
   # for full
   for(j in 1:min(c(length(lu.full[[1]]), length(lsky.full[[1]])))){
     for(k in 1:1024){
+      
       lufrac <- (lu.full[[1]][[j]][k,2]-lu.full[[1]][[j]][k,5])/(as.numeric(lu.full[[2]][[j]][which(lu.full[[2]][[j]] == "IT_VEG[us]=")+1,])*cal.full[k,2])
       result.full[k,1] <- lufrac
       
@@ -109,21 +143,52 @@ for(i in 1:length(myList)){
       
       lskyfrac <- ((p*(lsky.full[[1]][[j]][k,2]-lsky.full[[1]][[j]][k,5]))/(as.numeric(lsky.full[[2]][[j]][which(lu.full[[2]][[j]] == "IT_VEG[us]=")+1,])*cal.full[k,2]))
       result.full[k,2] <- lskyfrac
-
+      
       edfrac1 <- (lu.full[[1]][[j]][k,1]-lu.full[[1]][[j]][k,4])/(as.numeric(lu.full[[2]][[j]][which(lu.full[[2]][[j]] == "IT_WR[us]=")+1,])*cal.full[k,3])
       result.full[k,3] <- edfrac1
-
+      
       edfrac2 <- (lu.full[[1]][[j]][k,3]-lu.full[[1]][[j]][k,4])/(as.numeric(lu.full[[2]][[j]][which(lu.full[[2]][[j]] == "IT_WR[us]=")+1,])*cal.full[k,3])
       result.full[k,4] <- edfrac2
-
+      
       result1 <- (lufrac-lskyfrac)/edfrac1
       result.full[k,5] <- result1
-
+      
       result2 <- (lufrac-lskyfrac)/edfrac2
       result.full[k,6] <- result2
       
       result.full[k,7] <- cal.full[k,1]
+      
+      date <- lu.full[[2]][[j]][which(lu.full[[2]][[j]] == "GPS_date=")+1,]
+      time <- lu.full[[2]][[j]][which(lu.full[[2]][[j]] == "GPS_TIME_UTC=")+1,]
+      
+      result.full[k,8] <- date
+      
+      result.full[k,9] <- time
+      
+      lat <- lu.full[[2]][[j]][which(lu.full[[2]][[j]] == "GPS_lat=")+1,]
+      result.full[k,10] <- lat
+      
+      lon <- lu.full[[2]][[j]][which(lu.full[[2]][[j]] == "GPS_lon=")+1,]
+      result.full[k,11] <- lon
     }
-    write.csv(result.full, file = paste(getwd(), "/Results/", "results_full_", i ,"_", j, ".csv", sep=""), row.names = FALSE)
+    m = j + w
+    final.results.full[[m]] <- result.full
   }
+  Sys.sleep(1/1000)
+  setTxtProgressBar(pb.overall, i, title = NULL, label = NULL)
 }
+end.time <- Sys.time()
+paste("Running time of ", gsub("Time difference of", "", end.time - start.time), "s", sep = "")
+close(pb.overall)
+
+# aggregate lists and write them
+
+aggregate.fine <- do.call("rbind", lapply(final.results.fine, function(x) return(x)))
+
+aggregate.full <- do.call("rbind", lapply(final.results.full, function(x) return(x)))
+
+aggregate.fine <- aggregate.fine[c(8:11,7,1:6)]
+aggregate.full <- aggregate.full[c(8:11,7,1:6)]
+
+write.csv(aggregate.fine, "./Results/aggregateFine.csv")
+write.csv(aggregate.full, "./Results/aggregateFull.csv")
